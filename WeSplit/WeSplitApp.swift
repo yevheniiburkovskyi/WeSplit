1e02328c84ecc70eec0d03651d4212db8f48f925
Yevhenii Burkovskyi <yevhenii.burkovskyi@stormotion.io>
2024-04-11 11:32:19 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Yevhenii Burkovskyi on 11.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
